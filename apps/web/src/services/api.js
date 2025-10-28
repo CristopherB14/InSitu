2b21a5fc8a64e9e73f2d7343bb0ae60ba2d97a38
CristopherB14 <cristopheremma2007@gmail.com>
2025-10-28 00:07:45 -0300

    fix errors

--- apps/web/src/services/api.js
+++ apps/web/src/services/api.js
@@ -1,7 +1,14 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+// });
+
+// export default API;
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5127",
 });
 
 export default API;
